abb0ff58e04bd68401996e2016838cff6dedf112
Axel <atthehotcorner@users.noreply.github.com>
2014-02-05 17:24:09 -0500

    Update lcm.s

--- mips/assignment2/lcm.s
+++ mips/assignment2/lcm.s
@@ -3,8 +3,9 @@
 
 .data
 str1: .asciiz "Enter integer a\n"
-str2: .asciiz "Enter integer b\n "
+str2: .asciiz "Enter integer b\n"
 str3: .asciiz "The least common multiple of a and b is "
+nline: .asciiz "\n"
 
 .text
 main:
@@ -14,7 +15,7 @@ syscall			# print the string (a)
 
 li $v0, 5		# syscall 5 (read int, scanf)
 syscall			# get input
-move $t1, $v0		# place the value read into register $t1 (a=input, n1)
+move $t0, $v0		# place the value read into register $t1 (a=input, n1)
 
 li $v0, 4		# syscall 4 (printf)
 la $a0, str2		# argument: string
@@ -22,20 +23,37 @@ syscall			# print the string (b)
 
 li $v0, 5		# syscall 5 (read int, scanf)
 syscall			# get input
-move $t2, $v0		# place the value read into register $t2 (b=input, n2)
+move $t1, $v0		# place the value read into register $t2 (b=input, n2)
 
-jal lmc			# call procedure
-
+add $a0, $t0, $zero	# place n1 and n2 into args
+add $a1, $t1, $zero
+jal lcm			# call lcm
+move $t2, $v0		# save result ($t2)
 
 li $v0, 4		# syscall 4 (printf)
 la $a0, str3		# argument: string
-syscall			# print the string (lcm(n1, n2))
+syscall			# print the string (lcm of ...)
+
+li $v0, 1		# syscall 1 (print int)
+add $a0, $t2, $zero	# move int to $a0
+syscall			# print int (result of lcm call)
+
+li $v0, 4		# syscall 4 (printf)
+la $a0, nline		# argument: string
+syscall			# print the string (new line)
 
 li $v0, 10		# syscall 10 (terminate program, exit)
 syscall
 
-gcd:			# GCD procedure
+# Procedures
+lcm:			# args number1, number2		
+mult $a0, $a1		# number1 * number2
+mfhi $t3		# put hi in t3
+mflo $t4		# put lo in t4
+add $v0, $t3, $t4	# put return variable in $v0
+#jal gcd	
 jr $ra			# return from procedure
 
-lcm:			# LCM procedure
+
+gcd:			# GCD procedure
 jr $ra			# return from procedure
