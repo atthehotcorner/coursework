131445e7e35d7d0e316cfce833ea3afe505b8961
atthehotcorner <at.thehotcorner@gmail.com>
2014-03-12 00:22:52 -0400

    skips numbers

--- data-structures-COP3530/linkedBinaryTree/linkedBinaryTree.cpp
+++ data-structures-COP3530/linkedBinaryTree/linkedBinaryTree.cpp
@@ -5,24 +5,41 @@
 
 using namespace std;
 
-binaryTreeNode linkedBinaryTree::consturctTree(int *in, int *post, int startIndexIn, int endIndexIn, int startIndexPost, int endIndexPost) {
+binaryTreeNode* linkedBinaryTree::consturctTree(int *in, int *post, int startIndexIn, int endIndexIn, int startIndexPost, int endIndexPost) {
+	cout << startIndexIn << ", " << endIndexIn << ", " << startIndexPost << ", " << endIndexPost << endl;
+
 	// Empty array or only child, return and set NULL
-	if (endIndexIn - startIndexIn < 1 || endIndexPost - startIndexPost < 1) return NULL;
+	if (endIndexIn - startIndexIn < 1 || endIndexPost - startIndexPost < 1) {
+		cout << "\n";
+		return NULL;
+	}
 
 	// Create new node
 	binaryTreeNode *currentRoot = new binaryTreeNode(post[endIndexPost]);
 
 	// Find index of currentRoot in inorder
-	int currentRootInOrderIndex = 0;
+	int currentRootInOrderIndex = -1;
 	for (int i = startIndexIn; i <= endIndexIn; i++) {
 		if (in[i] == post[endIndexPost]) {
 			currentRootInOrderIndex = i;
+			cout << "(" << in[i] << ")\t";
 		}
+		else cout << in[i] << "\t";
 	}
+	cout << "\n";
+	for (int i = startIndexPost; i <= endIndexPost; i++) {
+		if (post[i] == post[endIndexPost]) {
+			cout << "(" << post[i] << ")\t";
+		}
+		else cout << post[i] << "\t";
+	}
+	cout << "\n" << endl;
 
 	// Call and set left and right
-	currentRoot->left = consturctTree(in, post, startIndexIn, currentRootInOrderIndex, startIndexPost, startIndexPost + currentRootInOrderIndex - startIndexIn);
-	currentRoot->right = consturctTree(in, post, currentRootInOrderIndex + 1, endIndexIn, startIndexPost + currentRootInOrderIndex - startIndexIn, endIndexPost - 1);
+	cout << "in left" << endl;
+	currentRoot->leftChild = consturctTree(in, post, startIndexIn, currentRootInOrderIndex - 1, startIndexPost, startIndexPost + (currentRootInOrderIndex - startIndexIn) - 1);
+	cout << "in right" << endl;
+	currentRoot->rightChild = consturctTree(in, post, currentRootInOrderIndex + 1, endIndexIn, startIndexPost + (currentRootInOrderIndex - startIndexIn), endIndexPost - 1);
 
 	return currentRoot;
 }
@@ -35,7 +52,7 @@ void linkedBinaryTree :: consturctTree(int *in, int *post)
 	int length = 17;
 
 	// begin from root
-	root = consturctTree(in, post, 0, length, 0, length);
+	root = consturctTree(in, post, 0, length - 1, 0, length - 1);
 }
 
 
