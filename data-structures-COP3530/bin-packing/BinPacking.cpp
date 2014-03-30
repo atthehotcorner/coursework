d1a26d4fd99a3a13ad206e02174f6a1a69036aaf
atthehotcorner <at.thehotcorner@gmail.com>
2014-03-29 23:37:31 -0400

    Uses root, will rewrite with dummy root

--- data-structures-COP3530/bin-packing/BinPacking.cpp
+++ data-structures-COP3530/bin-packing/BinPacking.cpp
@@ -23,7 +23,7 @@ using namespace std;
 
 void BinPacking::firstFitPack(int *objectSize, int numberOfObjects, int binCapacity)
 {
-	MaxWinnerTree mwtree = new MaxWinnerTree();
+	/*MaxWinnerTree mwtree = new MaxWinnerTree();
 
 	// players - number of bins
 	// value of player - capacity
@@ -59,28 +59,75 @@ void BinPacking::firstFitPack(int *objectSize, int numberOfObjects, int binCapac
    	 
 void BinPacking::bestFitPack(int *objectSize, int numberOfObjects, int binCapacity)
 {
+	cout << "== bin cap is " << binCapacity << " ==" << endl;
+	cout << "object set is { " << objectSize << " } with total number of " << numberOfObjects << endl << endl;
 
-	// init bins to one bin
-	numBin = 1;
-	binNumberArray = new int[numBin];
-
-	// make new bst
-	BinarySearchTree bst = new BinarySearchTree(binCapacity);
+	// make bst with dummy root as root is protected prop of binaryTree, no can modify
+	BinarySearchTree *bst;
 
 	// insert all objects
 	for (int i = 1; i <= numberOfObjects; i++) {
-		// there is no bin that can fit
-		if (!bst.find(objectSize[i - 1])) {
-			bst.insert(binCapacity, );
+		cout << "== object " << i << " size is " << objectSize[i-1] << " ==" << endl;
+
+		// 1st, make new tree
+		if (i == 1) {
+			// init bins to one bin
+			numBin = 1;
+			binNumberArray = new int[numBin];
+
+			// make new tree
+			bst = new BinarySearchTree(binCapacity - objectSize[i - 1]);
+			binNumberArray[i - 1] = binCapacity - objectSize[i - 1];
+
+			cout << "new tree, now using bin " << numBin << ", insert " << objectSize[i - 1] << endl;
 		}
 		// yes there is bin that fits
+		else if (bst->find(objectSize[i - 1]) != NULL || bst->root->capacity >= objectSize[i - 1]) {
+			int foundBinNumber = 0;
+			BinaryTreeNode *bin = bst->root;
+
+			// check if root has cap (error with find function, idk)
+			if (bst->root->capacity >= objectSize[i - 1]) foundBinNumber = 1;
+			// not, find in tree
+			else {
+				bin = bst->find(objectSize[i - 1]);
+				foundBin = returnedBin->binNumber; // ugh needed to change index offset this whole time
+			}
+			
+
+			// update capacity
+			int newCapacity = binNumberArray[foundBinNumber - 1] - objectSize[i - 1];
+			bin->capacity = newCapacity;
+			binNumberArray[foundBinNumber - 1] = newCapacity;
+
+			// remove bin from bst if cap is now zero
+			if (newCapacity == 0) bst->erase(bin);
+
+			cout << "yes has bin, now using bin " << foundBinNumber << ", insert " << objectSize[i - 1] << endl;
+		}
+		// there is no bin that can fit
 		else {
-			int returnedBinNum = bst.find(objectSize[i - 1]);
+			// make new bin, incement numBin by one and enlarge int array
+			numBin++;
+			int *newBinNumberArray = new int[numBin];
+			for (int j = 1; j < numBin; j++) {
+				newBinNumberArray[j - 1] = binNumberArray[j - 1];
+			}
+			newBinNumberArray[i - 1] = objectSize[i - 1];
+			binNumberArray = newBinNumberArray;
+
+			// insert into bst if not max cap filled
+			if (objectSize[i - 1] < binCapacity) bst->insert(binCapacity - objectSize[i - 1], numBin);
+
+			cout << "no bin found, now using bin " << numBin << ", insert " << objectSize[i - 1] << endl;
 		}
 
-	}
-	bst.find(objectSize[i]);
 
+		// debug print bin
+		for (int i = 0; i<numBin; i++) cout << binNumberArray[i] << "\t";
+		cout << endl;
+
+	}
 
 	/*
 	Binary Search Tree
@@ -88,11 +135,12 @@ void BinPacking::bestFitPack(int *objectSize, int numberOfObjects, int binCapaci
 	where objectSize is the array of objects to be packed, numberofObjects is the number of Objects to be
 	packed, binCapacity is the capacity of each bin.
 
- Items are packed one at a time in the given order.
- To determine the bin for an item, rst determine the set S of bins into which the item ts.
- If S is empty, then start a new bin and pack the item into this new bin.
- Otherwise, pack the item into the (in S) that has the least available capacity. If there is a tie, beak it with smallest binNumber.
- Use a Binary Search Tree to implement the Best Fit Algorithm for the same series of numbers.
+	 Items are packed one at a time in the given order.
+	 To determine the bin for an item, first determine the set S of bins into which the item fits.
+	 If S is empty, then start a new bin and pack the item into this new bin.
+	 Otherwise, pack the item into the (in S) that has the least available capacity. If there is a tie, beak it
+	with smallest binNumber.
+	 Use a Binary Search Tree to implement the Best Fit Algorithm for the same series of numbers.
 	*/
 	
 }
