4fc0bc620dbd7af8e957c9dc35c3e132e5224bf1
atthehotcorner <at.thehotcorner@gmail.com>
2014-03-30 12:24:47 -0400

    Completed First Fit Bin

--- data-structures-COP3530/bin-packing/BinPacking.cpp
+++ data-structures-COP3530/bin-packing/BinPacking.cpp
@@ -23,24 +23,24 @@ using namespace std;
 
 void BinPacking::firstFitPack(int *objectSize, int numberOfObjects, int binCapacity)
 {
-	/*MaxWinnerTree mwtree = new MaxWinnerTree();
+	// new nwtree
+	MaxWinnerTree *mwtree = new MaxWinnerTree();
 
 	// players - number of bins
 	// value of player - capacity
-
 	// init the maxwintree
-	mwtree.initialize(1, binCapacity);
+	mwtree->initialize(numBin, binCapacity);
 
-	/* idea of firstfitpack without tree
-	int binNum = 0;
+	for (int i = 0; i < numberOfObjects; i++) {
+		cout << "==i is " << i << "==" << endl;
+		BinaryTreeNode* currentObject = mwtree->find(objectSize[i]);
+		binNumberArray[i] = currentObject->binNumber;
+		cout << "We want to store object " << objectSize[i] << ", bin " << currentObject->binNumber << " found with cap of " << currentObject->capacity << endl;
 
-	int *currentBin = new int[binCapacity];
-	int currentBinSum = 0;
-
-	for (int i = 1; i <= numberOfObjects; i++) {
-		if
-		objectSize[i-1];
-	}*/
+		// change capacity and replay parent match
+		currentObject->capacity -= objectSize[i];
+		mwtree->replay(currentObject->parent);
+	}
 		
 	/*
 	Max Winner tree
@@ -56,83 +56,57 @@ void BinPacking::firstFitPack(int *objectSize, int numberOfObjects, int binCapac
 	*/
 
 }
-   	 
+
+/*BinarySearchTree* BinPacking::removeRoot(BinarySearchTree* bst) {
+
+}*/
+
 void BinPacking::bestFitPack(int *objectSize, int numberOfObjects, int binCapacity)
 {
 	cout << "== bin cap is " << binCapacity << " ==" << endl;
-	cout << "object set is { " << objectSize << " } with total number of " << numberOfObjects << endl << endl;
 
-	// make bst ref
-	BinarySearchTree *bst;
+	// Make BST stub
+	BinarySearchTree *bst = NULL;
+	int openBin = 1;
 
 	// insert all objects
 	for (int i = 1; i <= numberOfObjects; i++) {
 		cout << "== object " << i << " size is " << objectSize[i-1] << " ==" << endl;
 
-		// 1st, make new tree
-		if (i == 1) {
-			// init bins to one bin
-			numBin = 1;
-			binNumberArray = new int[numBin];
+		// Make tree if needed
+		if (bst == NULL && objectSize[i - 1] < binCapacity) bst = new BinarySearchTree(binCapacity - objectSize[i - 1]);
 
-			// make new tree
-			bst = new BinarySearchTree(binCapacity - objectSize[i - 1]);
-			binNumberArray[i - 1] = binCapacity - objectSize[i - 1];
+		// No bin fits object
+		if (!bst->find(objectSize[i - 1])) {
+			cout << "no fit" << endl;
+			// Modify array
+			binNumberArray[i - 1] = openBin;
 
-			cout << "new tree, now using bin " << numBin << ", insert " << objectSize[i - 1] << endl;
+			// Insert into BST if capacity not reached
+			if (objectSize[i - 1] < binCapacity) bst->insert(binCapacity - objectSize[i - 1], openBin);
+
+			openBin++;
 		}
-		// yes there is bin that fits
-		else if (bst->find(objectSize[i - 1]) != NULL || bst->root->capacity >= objectSize[i - 1]) {
-			int foundBinNumber = 0;
-
-			// check if root has cap (error with overwriting root, root is protected)
-			if (bst->root->capacity >= objectSize[i - 1]) {
-				foundBinNumber = 1;
-				bst->erase(bst->root);
-
-				// update capacity
-				int newCapacity = binNumberArray[foundBinNumber - 1] - objectSize[i - 1];
-				if (newCapacity != 0) bst->insert(binNumberArray[foundBinNumber - 1] - objectSize[i - 1], foundBinNumber);
-				binNumberArray[foundBinNumber - 1] = newCapacity;
-			}
-			// not, find in tree
-			else {
-				BinaryTreeNode *bin = bst->find(objectSize[i - 1]);
-				foundBinNumber = bin->binNumber; // ugh needed to change index offset this whole time
-
-				// update capacity
-				int newCapacity = binNumberArray[foundBinNumber - 1] - objectSize[i - 1];
-				bin->capacity = newCapacity;
-				binNumberArray[foundBinNumber - 1] = newCapacity;
-
-				// remove bin from bst if cap is now zero
-				if (newCapacity == 0) bst->erase(bin);
-			}
-
-			cout << "yes has bin, now using bin " << foundBinNumber << ", insert " << objectSize[i - 1] << endl;
-		}
-		// there is no bin that can fit
+		// Yes there is bin that fits object
 		else {
-			// make new bin, incement numBin by one and enlarge int array
-			numBin++;
-			int *newBinNumberArray = new int[numBin];
-			for (int j = 1; j < numBin; j++) {
-				newBinNumberArray[j - 1] = binNumberArray[j - 1];
-			}
-			newBinNumberArray[i - 1] = objectSize[i - 1];
-			binNumberArray = newBinNumberArray;
+			cout << "yes fit" << endl;
+			BinaryTreeNode *bin = bst->find(objectSize[i - 1]);
+			int foundBinNumber = bin->binNumber;
+			binNumberArray[i - 1] = foundBinNumber;
 
-			// insert into bst if not max cap filled
-			if (objectSize[i - 1] < binCapacity) bst->insert(binCapacity - objectSize[i - 1], numBin);
+			// Update capacity
+			int newCapacity = bin->capacity - objectSize[i - 1];
+			bin->capacity = newCapacity;
 
-			cout << "no bin found, now using bin " << numBin << ", insert " << objectSize[i - 1] << endl;
+			// Remove bin from BST if capacity is now zero
+			if (newCapacity == 0) bst->erase(bin);
 		}
-
 
 		// debug print bin
-		for (int i = 0; i<numBin; i++) cout << binNumberArray[i] << "\t";
+		for (int i = 0; i<numBin; i++) cout << "(" << binNumberArray[i] << ")\t";
 		cout << endl;
-
+		for (int j = 0; j<numberOfObjects; j++) cout << objectSize[j] << "\t";
+		cout << endl;
 	}
 
 	/*
